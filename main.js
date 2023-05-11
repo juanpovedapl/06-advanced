877fe8b2a071a306d3f08694d00601be3d69ed9a
juanpovedapl <juanpablopoveda93@gmail.com>
2023-05-11 15:43:37 -0500

    modificaciones minimas para arrancar

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -13,12 +12,11 @@ document.querySelector('#app').innerHTML = `
     </a>
     <h1>Hello Vite!</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+      
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+  </div>
+`;
+
+console.log('Hola mundo')
+
