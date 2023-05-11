0e8db71b76ec420eb77e8d748eabe8968768e6c6
juanpovedapl <juanpablopoveda93@gmail.com>
2023-05-11 16:57:00 -0500

    Se implemento el modulo callback y se hizo una funcion para explicar el funcionamiento de los callbacks

---
+++ src/concepts/02-callbacks.js
@@ -0,0 +1,26 @@
+import { heroes } from '../data/heroes';
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ *
+ */
+
+export const callbacksComponent = (element) => {
+
+    const id = '5d86371f25a058e5b1c8a65e';
+    findHero(id, (herByID) => {
+        element.innerHTML = herByID.name;
+    });
+}
+/**
+ *
+ * @param {String} id
+ * @param {(hero: Object) => void } callback
+ */
+const findHero = ( id, callback ) => {
+
+    const hero = heroes.find( hero => hero.id === id );
+    callback(hero);
+
+}
