2648f56aa4554db67923567ca9774f93cfd000e9
juanpovedapl <juanpablopoveda93@gmail.com>
2023-05-14 13:54:07 -0500

    En el modulo promise se creo una promesa que buscaba al hero por el id y renderizaba su nombre

--- src/concepts/03-promises.js
+++ src/concepts/03-promises.js
@@ -6,11 +6,43 @@ import {heroes} from '../data/heroes.js'
 
 export const promiseComponent = (element) => {
 
-    console.log('que chimba sog')
+    const renderHero = ( hero ) => {
+        element.innerHTML = hero.name;
+    }
+
+
+    renderTwoHeroes  = (hero1,hero2 ) => {
+        element.innerHTML = `
+            <h3>${hero1.name}</h3>
+            <h3>${hero2.name}</h3>
+            `;
+    }
+
+    const renderError = ( error) => {
+        element.innerHTML = `
+            <h1>Error: </h1>
+            <h3>${error}</h3>
+            `;
+    }
+    //usando promesa
+    const id1 = '5d86371f9f80b591f499df32';
+    const id2 = '5d86371f233c9f2425f16916';
+
+
+
+    findHero( id1 )
+        .then( renderHero)
+        //El codigo  comentado es equivalente a la linea anterior
+        //En caso de que la promesa llamada tenga como parametro
+        // una funcion con los mismos parametros de entrada, en el mismo
+        //orden en este caso se puede presindir de la variable superHero
+        // ya que  se pasaba como parametro de renderHero
+        //.then( superHero => renderHero(superHero));
+        .catch( renderError);
 }
 
 /**
- * 
+ *
  * @param {String} id
  * @returns {Promise}
  */
