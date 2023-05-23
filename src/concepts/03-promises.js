dd474e8b0b07f20884bf72289ef0d6e388f1daef
juanpovedapl <juanpablopoveda93@gmail.com>
2023-05-23 16:10:45 -0500

    se implementao una promesa

--- src/concepts/03-promises.js
+++ src/concepts/03-promises.js
@@ -28,23 +28,46 @@ export const promiseComponent = (element) => {
     const id1 = '5d86371f9f80b591f499df32';
     const id2 = '5d86371f233c9f2425f16916';
 
+    //! Forma 3. Cuando una promesa no depende  otra promesa anterior
+    // el then solo se ejecutra si  toas las promesas se cumplen correctamente.
+    Promise.all([
+        findHero(id1),
+        findHero(id2),
+    ])
+    .then(([hero1,hero2]) => renderTwoHeroes(hero1, hero2))
+    .catch(renderError);
+    //! Forma 2. Es más eficiente que la anterior y se usa cuando una promesa depende de otra
+    // let hero1;
 
-    findHero( id1 )
-        .then( (hero1) =>{
+    // findHero( id1 )
+    //     .then( (hero) =>{
+    //         hero1= hero;
+    //         return findHero(id2);
 
-            findHero( id2 )
-                .then(hero2 => {
-                    renderTwoHeroes(hero1,hero2)
-                })
-                .catch( renderError);
-        })
+    // })
+    // .then(hero2 => {
+    //     renderTwoHeroes(hero1,hero2);
+    // })
+    // .catch(renderError);
+
+    //! Forma 1. 
+    // findHero( id1 )
+    //     .then( (hero1) =>{
+
+    //         findHero( id2 )
+    //             .then(hero2 => {
+    //                 renderTwoHeroes(hero1,hero2)
+    //             })
+    //             .catch( renderError);
+    //     })
+    //     .catch( renderError);
         //El codigo  comentado es equivalente a la linea anterior
         //En caso de que la promesa llamada tenga como parametro
         // una funcion con los mismos parametros de entrada, en el mismo
         //orden en este caso se puede presindir de la variable superHero
         // ya que  se pasaba como parametro de renderHero
         //.then( superHero => renderHero(superHero));
-        .catch( renderError);
+        
 }
 
 /**
